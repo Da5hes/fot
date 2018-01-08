ac8aa51bbb2268b5f4a5122dfc5679c03dc0e0e1
FOT <fot.fuzz@gmail.com>
2018-01-08 14:14:58 +0800

    change order for fuzz/sem

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ BASEDIR=$(dirname "$0")/setup
 
 ${BASEDIR}/0-deps.sh
 ${BASEDIR}/1-instrument.sh
-${BASEDIR}/2-fuzz.sh
-${BASEDIR}/3-sem.sh
+${BASEDIR}/2-sem.sh
+${BASEDIR}/3-fuzz.sh
 ${BASEDIR}/4-ui.sh
