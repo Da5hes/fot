4ae1cfc62bd5e7b07b20dd83ad7844f0d75df016
FOT <fot.fuzz@gmail.com>
2018-02-09 21:33:25 +0800

    changed deb location

--- setup/1-instrument.sh
+++ setup/1-instrument.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
+# remove, even when there is NO
+sudo apt-get purge fot_tools -y
 _CUR_DIR=$(dirname "$0")
 source ${_CUR_DIR}/common.sh
-
 # fot-instrument
 fot_setup_header "fot-instrument"
 cd ${BASEDIR}/fot-instrument
