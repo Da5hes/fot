197c96f5977e23110fe0d8d755668b0f085bd0c1
FOT <fot.fuzz@gmail.com>
2018-04-17 11:15:44 +0800

    clean before building fot-fuzz

--- setup/3-fuzz.sh
+++ setup/3-fuzz.sh
@@ -8,6 +8,7 @@ set -e
 # fot-fuzz
 fot_setup_header "fot-fuzz"
 cd ${BASEDIR}/fot-fuzz
+cargo clean
 task install
 fot_setup_footer "fot-fuzz"
 fot_setup_alert "see how to use runtime fuzzing with 'fot-fuzz --help'"
