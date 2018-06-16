529e05afbed21bb5e39b99477ab6fba156a3105e
Hongxu <hongxuchen1989@gmail.com>
2018-06-16 22:39:51 +0800

    18.04

--- install.sh
+++ install.sh
@@ -3,8 +3,9 @@
 set -e
 
 BASEDIR=$(dirname "$0")/setup
+os=$(lsb_release -r -s)
 
-${BASEDIR}/0-deps.sh
+${BASEDIR}/0-deps-${os}.sh
 ${BASEDIR}/1-instrument.sh
 ${BASEDIR}/2-sem.sh
 ${BASEDIR}/3-fuzz.sh
