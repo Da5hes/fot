462abe135b1b20defa94c656cf470dfe6b71d345
Hongxu <hongxuchen1989@gmail.com>
2019-02-01 17:58:53 +0800

    bump version

--- setup/1-instrument.sh
+++ setup/1-instrument.sh
@@ -1,11 +1,33 @@
 #!/usr/bin/env bash
 
+set -e
+
 _CUR_DIR=$(dirname "$0")
 source ${_CUR_DIR}/common.sh
+
+export LLVM_DIR=/usr/lib/llvm
+
+# SVF
+fot_setup_header "SVF"
+cd ${BASEDIR}/SVF
+BUILDDIR=${BASEDIR}/SVF/BUILD_RELDBG
+if ! [[ -d $BUILDDIR ]]; then
+    mkdir $BUILDDIR
+fi
+cd $BUILDDIR
+cmake -GNinja -DCMAKE_BUILD_TYPE=RelWithDebInfo ${BASEDIR}/SVF
+ninja
+sudo ninja install
+cd ${BASEDIR}
+fot_setup_footer "SVF"
+
 # fot-instrument
 fot_setup_header "fot-instrument"
 cd ${BASEDIR}/fot-instrument
 sudo make uninstall
 make
 sudo make install
+cd ${BASEDIR}
 fot_setup_footer "fot-instrument"
+
+set +e
