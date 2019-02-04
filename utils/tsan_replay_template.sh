80c9461e358c082a7ef7ff0a819d3d955e9141d2
Hongxu <hongxuchen1989@gmail.com>
2019-02-04 11:44:20 +0800

    misc

--- utils/tsan_replay_template.sh
+++ utils/tsan_replay_template.sh
@@ -4,21 +4,22 @@ EXT="res"
 
 # set -e
 
-if [ "$#" -lt 1 ]; then
-    echo "usage: $0 REPLAY_DIR"
+if [ "$#" -lt 2 ]; then
+    echo "usage: $0 REPLAY_DIR LOGFILE"
     exit 1
 fi
 
 REPLAY_DIR=$1
+LOGFILE=$2
 
 for f in $REPLAY_DIR/*; do 
-    ext=="${f##*.}";
+    ext="${f##*.}";
     if [[ "$ext" == "$EXT" ]]; then
         echo "Ignore $f"
     else
         echo "===$f"
         time timeout -s 9 60 ./ImageMagick-tsan/install/bin/convert -quiet $f /tmp/tsan.bmp 2>$f.${EXT}
     fi
-done | tee REPLAY 
+done | tee $LOGFILE
 
 # set +e
