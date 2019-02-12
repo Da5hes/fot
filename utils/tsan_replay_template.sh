c8948014f901de6af191832ff9834dfbe74a3836
Hongxu <hongxuchen1989@gmail.com>
2019-02-12 15:16:04 +0800

    replay to halt for tsan

--- utils/tsan_replay_template.sh
+++ utils/tsan_replay_template.sh
@@ -18,7 +18,7 @@ for f in $REPLAY_DIR/*; do
         echo "Ignore $f"
     else
         echo "===$f"
-        time timeout -s 9 60 ./ImageMagick-tsan/install/bin/convert -quiet $f /tmp/tsan.bmp 2>$f.${EXT}
+        time timeout -s 9 30 ./ImageMagick-tsan/install/bin/convert -quiet $f /tmp/tsan.bmp 2>$f.${EXT}
     fi
 done | tee $LOGFILE
 
