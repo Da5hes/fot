651849239eda4340a3e625b85b74267aa1803a41
Hongxu <hongxuchen1989@gmail.com>
2019-03-17 17:33:03 +0800

    setup scripts

--- setup/0-deps.sh
+++ setup/0-deps.sh
@@ -5,12 +5,21 @@ source ${_CUR_DIR}/common.sh
 
 set -e
 
+os=$(lsb_release -r -s)
+if [[ $os != "18.04" ]] || [[ $os != "16.04" ]]; then
+    echo "unsupported OS version: $os"
+fi
+
 LLVMVER=7
 
 fot_setup_header "dependencies"
 fot_setup_header "OS-level dependencies"
+
+if [[ $os == "16.04" ]]; then
+    sudo add-apt-repository ppa:longsleep/golang-backports -y
+fi
+
 sudo add-apt-repository ppa:webupd8team/java -y
-sudo add-apt-repository ppa:longsleep/golang-backports -y
 sudo apt-get update
 sudo apt-get install -y cmake \
                         python-pip \
@@ -20,15 +29,17 @@ sudo apt-get install -y cmake \
                         liblzma-dev \
                         libhwloc-dev \
                         libc6-dev-i386 \
+			            libssl-dev \
                         curl \
                         realpath \
                         scons \
-                        software-properties-common \
-                        python-software-properties \
                         git \
                         clang-${LLVMVER} \
                         oracle-java8-installer
 
+if [[ $os == "16.04" ]]; then
+    sudo apt-get install -y software-properties-common python-software-properties
+fi
 
 sudo ln -sf /usr/lib/jvm/java-8-oracle/jre/lib/amd64/server/libjvm.so /usr/lib/libjvm.so 
 sudo ln -sf /usr/bin/clang-${LLVMVER} /usr/bin/clang
@@ -40,16 +51,25 @@ fot_setup_footer "OS-level dependencies"
 
 # go
 fot_setup_header "golang dependencies"
+if [[ ! -z "$GOPATH" ]]; then
+    fot_setup_alert "PLEASE 'export GOPATH=\$HOME/gocode' and make sure \$GOPATH/bin is in \$PATH"
+    exit 1
+fi
+# go task
 if ! hash task 2>/dev/null; then
-    export GOPATH=$HOME/gocode
-    fot_setup_alert "PLEASE 'export GOPATH=\$HOME/gocode' for permanent uses"
     mkdir -p $GOPATH
     go get -u -v github.com/go-task/task/cmd/task
-    export PATH=$GOPATH/bin:$PATH
     fot_setup_ensure_exec "task"
     fot_setup_alert "PLEASE 'export PATH=\$GOPATH/bin:\$PATH' for permanent uses"
 fi
 fot_setup_ensure_exec "task"
+# gllvm
+if ! hash gclang 2>/dev/null; then
+    mkdir -p $GOPATH
+    go get -u -v github.com/SRI-CSL/gllvm/cmd/...
+    fot_setup_ensure_exec "gclang"
+    fot_setup_ensure_exec "gclang++"
+fi
 fot_setup_footer "golang dependencies"
 
 # rust
